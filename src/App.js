4167fe511dbb483a47eee737ba62f48d75ac8923
Міша <64218817+mishok321@users.noreply.github.com>
2023-06-23 21:12:52 +0300

    [dependencies] added initial dependencies (#2)
    * added scss to the project
    
    * purge redunant files & added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div class="App-header-capture">This is new capture!</div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">1</div>
+          <div className="col-4">2</div>
+          <div className="col-4">3</div>
+        </div>
+      </div>
     </div>
   );
 }
