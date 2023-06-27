391d8026c5c160d82c9e9f8b0ced4096880a2a50
Bogdan <Bogdan@DESKTOP-PGENPK7>
2023-06-27 13:56:27 +0300

    added authorize

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './scss/App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Register from "./components/Register";
-import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
         {/* Header */}
-        <Login/>
+        <Register/>
         {/*  Footer  */}
     </div>
 
